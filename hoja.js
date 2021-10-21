f54eebe18a344d15563eb8f4af2d8d84ac9250e9
Lulytass <88911233+Lulytass@users.noreply.github.com>
2021-10-21 17:25:52 -0300

    clase 9
    agregue hoja de JS con apertura y cierre que opera sobre el botón agregado en el footer agregado en html, también se agrego un alert antes de que se cargue el DOM. quite la clase width en html de los modales de la sección "más recetas" para que cuando se visualiza en dispositivos móviles ocupen la pantalla completa

---
+++ hoja.js
@@ -0,0 +1,9 @@
+const modalInfo = document.querySelector("#modalInfo");
+
+const abrirModal = () =>{
+  modalInfo.style.display = "flex"
+}
+
+const cerrarModal = () =>{
+  modalInfo.style.display = "none"
+}
